6faa4cb8a8b93d04eff611527612c971eef747de
jaaeee <jmillar@ualberta.ca>
2019-08-15 22:36:10 -0700

    Initial Commit

---
+++ nowaste/static/build/precache-manifest.bffcbb3dcf6b67dabd47acdd983a697b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92594fe83aab6210f12e5d7eae929ee2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c84ea6a70ed22c0e2e3d",
+    "url": "./static/css/main.3769c8cf.chunk.css"
+  },
+  {
+    "revision": "e105bc63936e012799a1",
+    "url": "./static/js/2.7c297b2e.chunk.js"
+  },
+  {
+    "revision": "c84ea6a70ed22c0e2e3d",
+    "url": "./static/js/main.9b2705b6.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
